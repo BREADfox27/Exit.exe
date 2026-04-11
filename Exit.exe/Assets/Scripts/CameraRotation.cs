3f462b6544d4b01b09353a7fbaac22f05a0c9a1c
luciallanes12-design <luciallanes12@gmail.com>
2026-04-11 18:42:17 +0200

    Blockout del Escenario y movimiento del Player

---
+++ Exit.exe/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+
+
+public class CameraRotation : MonoBehaviour
+{
+    public float mouseSensitivity = 80f;
+    public Transform player;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void MouseRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+
+    void Update()
+    {
+        MouseRotation();
+    }
+}
+
---
+++ Exit.exe/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController playercontroller;
+    public float speed = 10f;
+
+    float gravity = -9.81f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float sphereRadius = 0.3f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+    public float jump = 3;
+    void Update()
+    {
+        CharacterMovement();
+        CharacterJump();
+    }
+
+    void CharacterMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        playercontroller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        playercontroller.Move(velocity * Time.deltaTime);
+    }
+
+    void CharacterJump()
+    {
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, sphereRadius, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jump * -2  * gravity);
+        }
+    }
+}
+
