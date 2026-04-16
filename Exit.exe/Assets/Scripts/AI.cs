3b16162edbeb6202e370e9b41d5db00b08f54367
BREADfox27 <javierluna271107@gmail.com>
2026-04-16 19:20:37 +0200

    Enemy AI

---
+++ Exit.exe/Assets/Scripts/AI.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+
+    public Transform[] DestinationPoints;
+
+    public float distanceToFollowPath = 2;
+
+    int i = 0;
+
+    public bool followPlayer;
+    GameObject player;
+    float distanceToPlayer;
+
+    public float distanceToFollow = 10;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        i = 0;
+        navMeshAgent.destination = DestinationPoints[i].position;
+        player = FindFirstObjectByType<PlayerController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= distanceToFollow && followPlayer)
+        {
+            FollowPlayer();
+        }
+
+        else
+        {
+            EnemyPath();
+        }
+    }
+
+    public void EnemyPath()
+    {
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance <= distanceToFollowPath)
+        {
+            if (i < DestinationPoints.Length - 1)
+            {
+                i++;
+            }
+
+            else
+            {
+                i = 0;
+            }
+
+            navMeshAgent.destination = DestinationPoints[i].position;
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        navMeshAgent.destination = player.transform.position;
+    }
+}
