a52c10cebb623e2a42dce42ac8ec3b1cfe9fd304
BREADfox27 <javierluna271107@gmail.com>
2026-04-21 16:54:04 +0200

    Raycast avances

--- Exit.exe/Assets/Scripts/PlayerController.cs
+++ Exit.exe/Assets/Scripts/PlayerController.cs
@@ -73,7 +73,7 @@ public class PlayerController : MonoBehaviour
             {
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    
+                    hit.collider.gameObject.SetActive(false);
                 }
             }
         }
